8fc8d162800a807bda2eebb90434c60a6a5b27f3
puneeth <puneethmotukuru@gmail.com>
2021-05-03 20:23:59 +0530

    #7 add get request

--- server/index.ts
+++ server/index.ts
@@ -3,8 +3,10 @@ import * as cors from "cors"
 require('dotenv').config()
 const app=express()
 app.use(cors())
-const PORT=process.env.REACT_PORT || 3000
-
+const PORT=process.env.REACT_PORT || 3333
+app.get("/api",(req,res)=>{
+    res.send("Welcome to backend API")
+})
 app.listen(PORT,()=>{
     console.log(` server running on ${PORT}`)
 })
